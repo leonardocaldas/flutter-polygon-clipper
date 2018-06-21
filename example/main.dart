bec1cbb6116577cdea156a5385079f949506a2f1
Leonardo Caldas <leonardo.caldas@zup.com.br>
2018-06-21 13:37:09 -0300

    adding example

---
+++ example/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_polygon_clipper/polygon_clipper.dart';
+
+void main() => runApp(new ExampleApp());
+
+class ExampleApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      color: Colors.white,
+      child: new Center(
+        child: new ClipPolygon(
+          child: new Container(
+              decoration: new BoxDecoration(
+                gradient: new LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Colors.red[200],
+                    Colors.red[800],
+                  ],
+                ),
+              ),
+              child: new Center(
+                child: new Icon(
+                  Icons.add_a_photo,
+                  color: Colors.white,
+                  size: 150.0,
+                  textDirection: TextDirection.ltr,
+                ),
+              )
+          ),
+          boxShadows: [
+            new PolygonBoxShadow(color: Colors.black, elevation: 5.0),
+          ],
+          sides: 6,
+          borderRadius: 5.0,
+        ),
+      ),
+    );
+  }
+}
